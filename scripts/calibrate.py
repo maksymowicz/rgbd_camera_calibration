b513acc92110e744b8795e1ae8c3c9e8e31398dc
Armon Shariati <armon.shariati@gmail.com>
2016-08-26 17:21:52 -0400

    Being more frank with reading files

--- scripts/calibrate.py
+++ scripts/calibrate.py
@@ -7,6 +7,7 @@ import numpy as np
 import glob
 import cv2
 import pprint
+import csv
 
 import cv_bridge
 import rosbag
@@ -14,7 +15,6 @@ import sensor_msgs
 import sensor_msgs.msg
 
 def load_images(f):
-
 
     # bag file 
     bag = rosbag.Bag(f, 'r')
@@ -38,6 +38,25 @@ def load_images(f):
 
     return I_rgb, I_depth
 
+def load_ground_truth(f):
+
+    gt = []
+    with open(f, 'r') as fstream:
+
+        # strip first line
+        header = fstream.readline()
+
+        # for each line
+        for line in fstream:
+
+            # clean the junk
+            cleanline = line.replace('\n', '').replace(' ', '')
+
+            # parse
+            gt.append(cleanline.split(',')[1:-1])
+
+    return np.array(gt)
+
 def load_intrinsics(f):
 
     # parse
@@ -58,34 +77,26 @@ def mouse_callback(event, x, y, flags, param):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 2:
-        print 'Usage: ' + os.path.basename(sys.argv[0]) + ' <directory>'
+    if len(sys.argv) < 5:
+        print 'Usage: ' + os.path.basename(sys.argv[0]) + \
+                ' <rgb_calibration.yaml>' + ' <depth_calibration.yaml>' + \
+                ' <ground_truth.txt>' + ' <bag_file.bag>'
         sys.exit()
 
-    # get absolute path
-    abspath = os.getcwd() + '/' + sys.argv[1]
-
     # intrinsic calibrations
-    C = {}
+    Kr = load_intrinsics(sys.argv[1])
+    Kd = load_intrinsics(sys.argv[2])
 
-    # for each yaml file in the directory
-    for f in glob.glob(abspath + '*.yaml'):
-
-        # intrinsics type
-        t = f.split('/')[-1].split('_')[0]
-
-        C[t] = load_intrinsics(f)
-
-    # pprint.pprint(C)
+    # load ground truth
+    gt = load_ground_truth(sys.argv[3])
 
     # load the images from the bag file
-    f = glob.glob(abspath + '*.bag')[0]
-    [I_rgb, I_depth] = load_images(f)
+    [I_rgb, I_depth] = load_images(sys.argv[4])
 
+    # 
     cv2.namedWindow('image')
     cv2.setMouseCallback('image', mouse_callback)
-
-    cv2.imshow('image', I_rgb)
+    cv2.imshow('image', 256 * I_depth)
     cv2.waitKey(0)
 
 
